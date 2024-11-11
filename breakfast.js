dca211634720fed20013246c39c8fc54e27cbc34
Lenagran18 <Lenagran18@gmail.com>
2024-11-11 15:52:09 -0500

    Delete breakfast files to restructure

--- breakfast.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("close-button").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.close();
-});
-
-document.getElementById("back-button").addEventListener("click", function (event) {
-    event.preventDefault();
-    window.location.href = "popup.html";
-});
